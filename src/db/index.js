aa736cb37d6cc0e1094c571136dfb0ad72435a85
yuvraj singh sisodiya <uvrajput0107@gmail.com>
2025-07-20 11:19:01 +0530

    DB connected

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose, { connect } from "mongoose";
+
+import { DB_NAME } from "../constents.js";
+
+const connectDB = async () => {
+  try {
+    const connectioninstence = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    console.log(`MongoDB connected : conection host : ${connectioninstence.connection.host}`);
+    
+  } catch (err) {
+    console.log("ERROR: in connection DB : ",err);
+    process.exit(1)
+  }
+};
+
+
+export default connectDB;
