c581a3ed4684e086dc13005002e592516d9374b9
yuvraj singh sisodiya <uvrajput0107@gmail.com>
2025-07-20 13:44:22 +0530

    reduce file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,50 @@
 // require('dotenv').config({path : './env'})
 
 import dotenv from "dotenv";
-import connectDB from './db/index.js';
 dotenv.config({path : './.env'});
 
-connectDB();
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
+
+import app from './app.js';
+import connectDB from './db/index.js';
+
+
+// configure cors
+app.use(cors({
+  origin: process.env.CORS_ORIGIN
+}))
+
+// configure json
+app.use(express.json({limit : '16kb'}))
+// configure url encoder
+app.use(express.urlencoded({extended: true, limit : '16kb'}))
+// configure static files 
+app.use(express.static('public'))
+// configure cookies
+app.use(express.cookieParser())
+
+
+
+
+
+connectDB()
+.then(() => {
+  app.on('error', (error) => {
+    console.log(`error in connction surver to DB : ${error}`);
+  })
+
+  app.listen(process.env.PORT || 8000 , () => {
+    console.log(`listnig on port : ${process.env.PORT}  vist => http://localhost:${process.env.PORT}`);
+    
+  })
+
+})
+.catch((err) => {
+  console.log("unable to connect to db")
+  throw err
+})
 
 
 
